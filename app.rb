e2f6262574f42c26996148e4f57526487235aba5
Omar Khan <omarkhan@Omars-MacBook-Pro.local>
2018-10-29 12:12:33 +0000

    set up

--- app.rb
+++ app.rb
@@ -1,1 +1,10 @@
-require 'pg'
+require 'sinatra/base'
+
+class ChitterChallenge < Sinatra::Base
+
+  get '/' do
+    'Chitter Challenge'
+  end
+
+  run! if app_file == $0
+end
---
+++ spec/features/viewing_chitter_page.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+
+feature 'Viewing Chitter' do
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content "Chitter Challenge"
+  end
+end
