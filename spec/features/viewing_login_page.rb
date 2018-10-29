34b6b6dcfb1397f36b7d17b9276b4e606e1c5a92
Omar Khan <omarkhan@Omars-MacBook-Pro.local>
2018-10-29 14:47:43 +0000

    login form

--- spec/features/viewing_login_page.rb
+++ spec/features/viewing_login_page.rb
@@ -3,4 +3,10 @@ feature 'Viewing Chitter login page' do
     visit('/login')
     expect(page).to have_content "This is the login page"
   end
+
+  scenario 'entering information for login page' do
+    visit('/login')
+    fill_in('Username', with: 'Max')
+    click_button('Submit')
 end
+end 
