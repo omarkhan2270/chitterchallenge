3ff308e4b56fd759d93bac9758424f30641e6540
Max Francis <43236116+MaxFrancis1@users.noreply.github.com>
2018-10-31 09:10:49 +0000

    Update signing_up.rb

--- spec/features/signing_up.rb
+++ spec/features/signing_up.rb
@@ -3,7 +3,7 @@ feature 'Signing up' do
     visit('/login')
     fill_in('Sign up Username', with: 'Max Francis')
     fill_in('Sign up Password', with: '1234')
-    fill_in('Sign up email', with: 'Max.francis@cgi.com')
+    fill_in('Sign up email', with: 'Max.francis@gmail.com')
     click_button('Sign up Submit')
     expect(page).to have_content "This is the login page"
   end
